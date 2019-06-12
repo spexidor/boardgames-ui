83d221c544d031aed9e4a44fbe4dc05027fe23e0
Jonas Henriksson <jonas.henriksson.1849@gmail.com>
2019-06-12 15:22:28 +0200

    jenkins11

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -15,9 +15,6 @@ echo "starting new container"
 
 JENKINS_NODE_COOKIE=dontkillme docker run -d  \
   --name kdm-frontend-dc \
-  -v ${PWD}:/usr/src/app \
-  -v /usr/src/app/node_modules \
   -p 5000:5000 \
-  --privileged \
   --rm \
   kdm-frontend-dc
