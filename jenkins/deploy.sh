b1c85e4c4d070786654c99fa0d6c41219a0aa44d
Jonas Henriksson <jonas.henriksson.1849@gmail.com>
2019-06-12 13:24:03 +0200

    jenkins6

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,20 +1,21 @@
 echo "starting awesome react docker deployment script"
 
 echo "stopping old container"
-docker stop kdm-frontend
+#docker stop kdm-frontend-dc
 
 echo "building new image"
-docker build -t kdm-frontend .
+#docker build -t kdm-frontend-dc .
 
-echo "whoami: " whoami
+echo "whoami: " 
+whoami
 echo "PWD: " ${PWD}
 echo "starting new container"
 
 JENKINS_NODE_COOKIE=dontkillme docker run -d  \
-  --name kdm-frontend \
+  --name kdm-frontend-dc \
   -v ${PWD}:/usr/src/app \
   -v /usr/src/app/node_modules \
   -u $(id -g) \
   -p 5000:5000 \
   --rm \
-  kdm-frontend
+  kdm-frontend-dc
