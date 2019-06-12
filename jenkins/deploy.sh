ba25361073bda3adb88d410e49f1d116f252f73b
Jonas Henriksson <jonas.henriksson.1849@gmail.com>
2019-06-12 13:26:25 +0200

    jenkins7

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -4,18 +4,19 @@ echo "stopping old container"
 #docker stop kdm-frontend-dc
 
 echo "building new image"
-#docker build -t kdm-frontend-dc .
+docker build -t kdm-frontend-dc .
 
 echo "whoami: " 
 whoami
 echo "PWD: " ${PWD}
+echo "ls:"
+ls -l
 echo "starting new container"
 
 JENKINS_NODE_COOKIE=dontkillme docker run -d  \
   --name kdm-frontend-dc \
   -v ${PWD}:/usr/src/app \
   -v /usr/src/app/node_modules \
-  -u $(id -g) \
   -p 5000:5000 \
   --rm \
   kdm-frontend-dc
